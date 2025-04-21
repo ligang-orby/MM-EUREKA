e11873edfa7d6535a879f5f561a8c464ad45c428
Gang Li <ligang@orby.ai>
2025-04-21 06:35:46 +0000

    Fix dataset

--- examples/scripts/modify_image_path.py
+++ examples/scripts/modify_image_path.py
@@ -9,8 +9,15 @@ with open(out_data_path, 'w') as of:
             example = json.loads(l)
             image_urls = example['image_urls']
             new_urls = ['file:///MM-Eureka-Dataset/' + url for url in image_urls]
-            example['image_urls'] = new_urls
-            example['message'] = example['conversations']
+            images = [{"type": "image", "image": url} for url in new_urls]
+            message = example['conversations']
+            message = [m for m in message if m['role'] == 'user']
+            message = [{"type": "text", "text": m['content'].replace('<image>', '').strip()} for m in message]
+            message = images + message
+            message = [{'role': 'user', 'content': message}]            
+            example['message'] = json.dumps(message)
+            del example['image_urls']
+            del example['conversations']
             of.write(json.dumps(example) + '\n')
 
 
